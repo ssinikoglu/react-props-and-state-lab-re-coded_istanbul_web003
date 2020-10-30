482570b125f55956e5e64172ab84646c36271361
Sule Sinikoglu <ssinikoglu@gmail.com>
2020-10-30 14:19:23 +0300

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,55 @@
-import React from 'react'
+import React from "react";
 
-import Filters from './Filters'
-import PetBrowser from './PetBrowser'
+import Filters from "./Filters";
+import PetBrowser from "./PetBrowser";
 
 class App extends React.Component {
   constructor() {
-    super()
+    super();
 
     this.state = {
       pets: [],
       filters: {
-        type: 'all'
-      }
-    }
+        type: "all",
+      },
+    };
   }
+
+  onChangeType = (e) => {
+    this.setState({
+      filters: {
+        type: e.target.value,
+      },
+    });
+  };
+
+  fetchData = (url) => {
+    return fetch(url)
+      .then((res) => res.json())
+      .then((data) => this.setState({ pets: data }));
+  };
+
+  fetchPets = (e) => {
+    if (this.state.filters.type === "cat") {
+      this.fetchData("/api/pets?type=cat");
+    } else if (this.state.filters.type === "dog") {
+      this.fetchData("/api/pets?type=dog");
+    } else if (this.state.filters.type === "micropig") {
+      this.fetchData("/api/pets?type=micropig");
+    } else {
+      this.fetchData("/api/pets");
+    }
+  };
+
+  onAdoptPet = (id) => {
+    const pets = this.state.pets.map((pet) => {
+      if (pet.id === id) {
+        return { ...pet, isAdopted: true };
+      }
+      return pet;
+    });
+    this.setState({ pets: pets });
+  };
 
   render() {
     return (
@@ -24,16 +60,19 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters
+                onChangeType={this.onChangeType}
+                onFindPetsClick={this.fetchPets}
+              />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet} />
             </div>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
